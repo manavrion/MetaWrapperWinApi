2d2d0b69738f099dfaa6cb3d7266e4f7fd9d339a
manavrion <manavrion@yandex.ru>
2017-01-05 02:10:34 +0500

    added getters and setters to AbstructFramePositioning fields

--- MetaWrapperWinApi/AbstructFramePositioning.h
+++ MetaWrapperWinApi/AbstructFramePositioning.h
@@ -35,27 +35,131 @@ namespace MetaFrame {
         HorizontalAlignment horizontalAlignment;
         VerticalAlignment verticalAlignment;
 
-        /*AbstructFramePositioning *setRect(Rect rect);
+    public:
+        //setters
+        AbstructFramePositioning *setRect(Rect rect) {
+            x = rect.x;
+            y = rect.y;
+            width = rect.width;
+            height = rect.height;
+            return this;
+        };
 
-        AbstructFramePositioning *setX(int x);
-        AbstructFramePositioning *setY(int y);
-        AbstructFramePositioning *setSize(Size size);
-        AbstructFramePositioning *setWidth(int width);
-        AbstructFramePositioning *setHeight(int height);
-        AbstructFramePositioning *setAutoWidth(bool autoWidth);
-        AbstructFramePositioning *setAutoHeight(bool autoHeight);
-        AbstructFramePositioning *setMinWidth(int minWidth);
-        AbstructFramePositioning *setMaxWidth(int maxWidth);
-        AbstructFramePositioning *setMinHeight(int minHeight);
-        AbstructFramePositioning *setMaxHeight(int maxHeight);
+        AbstructFramePositioning *setX(int x) {
+            this->x = x;
+            return this;
+        };
+        AbstructFramePositioning *setY(int y) {
+            this->y = y;
+            return this;
+        };
+        AbstructFramePositioning *setSize(Size size) {
+            height = size.height;
+            width = size.width;
+            return this;
+        };
+        AbstructFramePositioning *setWidth(int width) {
+            this->width = width;
+            return this;
+        };
+        AbstructFramePositioning *setHeight(int height) {
+            this->height = height;
+            return this;
+        };
+        AbstructFramePositioning *setAutoWidth(bool autoWidth) {
+            this->autoWidth = autoWidth;
+            return this;
+        };
+        AbstructFramePositioning *setAutoHeight(bool autoHeight){
+            this->autoHeight = autoHeight;
+            return this;
+        };
+        AbstructFramePositioning *setMinWidth(int minWidth){
+            this->minWidth = minWidth;
+            return this;
+        };
+        AbstructFramePositioning *setMaxWidth(int maxWidth){
+            this->maxWidth = maxWidth;
+            return this;
+        };
+        AbstructFramePositioning *setMinHeight(int minHeight){
+            this->minHeight = minHeight;
+            return this;
+        };
+        AbstructFramePositioning *setMaxHeight(int maxHeight){
+            this->maxHeight = maxHeight;
+            return this;
+        };
                                  
-        AbstructFramePositioning *setMargin(int left, int right, int top, int bottom);
-        AbstructFramePositioning *setMargin(Margin margin);
+        AbstructFramePositioning *setMargin(int left, int right, int top, int bottom) {
+            this->margin = Margin(left, right, top, bottom);
+            return this;
+        };
+        AbstructFramePositioning *setMargin(Margin margin) {
+            this->margin = margin;
+            return this;
+        };
                                  
-        AbstructFramePositioning *setHorizontalAlignment(HorizontalAlignment horizontalAlignment);
-        AbstructFramePositioning *setVerticalAlignment(VerticalAlignment verticalAlignment);*/
+        AbstructFramePositioning *setHorizontalAlignment(HorizontalAlignment horizontalAlignment) {
+            this->horizontalAlignment = horizontalAlignment;
+            return this;
+        };
+        AbstructFramePositioning *setVerticalAlignment(VerticalAlignment verticalAlignment) {
+            this->verticalAlignment = verticalAlignment;
+            return this;
+        };
 
+        //getters
+        int getX() {
+            return x;
+        };
+        int getY() {
+            return y;
+        };
 
+        Rect getRect() {
+            return Rect(x, y, width, height);
+        };
+
+        int getWidth() {
+            return width;
+        };
+        int getHeight() {
+            return height;
+        };
+
+        Size getSize() {
+            return Size(width, height);
+        };
+        bool getAutoWidth() {
+            return autoWidth;
+        };
+        bool getAutoHeight() {
+            return autoHeight;
+        };
+        int getMinWidth() {
+            return minWidth;
+        };
+        int getMaxWidth() {
+            return maxWidth;
+        };
+        int getMinHeight() {
+            return minHeight;
+        };
+        int getMaxHeight() {
+            return maxHeight;
+        };
+
+        Margin getMargin() {
+            return margin;
+        };
+
+        HorizontalAlignment getHorizontalAlignment() {
+            return horizontalAlignment;
+        };
+        VerticalAlignment getVerticalAlignment() {
+            return verticalAlignment;
+        };
 
     public:
         ~AbstructFramePositioning() {};
