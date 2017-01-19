91d2e97c3eb03016f359c17bed86c47f82a7984c
manavrion <manavrion@yandex.ru>
2017-01-19 06:47:44 +0500

    added loading

--- MetaWrapperWinApi/Editor.cpp
+++ MetaWrapperWinApi/Editor.cpp
@@ -178,6 +178,116 @@ namespace MetaFrame {
         editorSpace->repaint();
     }
 
+
+
+
+    String s;
+    struct Prop {
+        int x, y, w, h;
+        String text;
+    };
+
+
+    int nextInt() {
+        String ret;
+        while (!s.empty()) {
+            if (!isalnum(s.front())) {
+                s.erase(0, 1);
+            } else {
+                break;
+            }
+        }
+        while (!s.empty()) {            
+            if (isalnum(s.front())) {
+                ret += s.front();
+                s.erase(0, 1);
+            } else {
+                break;
+            }
+        }
+        return ret.toValueInt();
+    }
+
+    String nextStr() {
+        String ret;
+        while (!s.empty()) {
+            if (s.front() != L'L') {
+                s.erase(0, 1);
+            } else {
+                s.erase(0, 1);
+                s.erase(0, 1);
+                break;
+            }
+        }
+        while (!s.empty()) {
+            if (s.front() != L'\"') {
+                ret += s.front();
+                s.erase(0, 1);
+            } else {
+                s.erase(0, 1);
+                s.erase(0, 1);
+                break;
+            }
+        }
+        return ret;
+    }
+
+
+    Prop nextProperty() {
+        s.erase(0, s.find(L"setProperty(") + 12);
+
+        Prop p;
+
+        p.x = nextInt();
+        p.y = nextInt();
+        p.w = nextInt();
+        p.h = nextInt();
+        p.text = nextStr();
+        return p;
+    }
+
+
+    void loadelem(FrameObject *editorSpace) {
+        FrameObject *obj;
+
+        s.erase(0, 5);
+        if (s.front() == L'P') {
+            obj = new Panel;
+            obj->initString = L"(new Panel)";
+        } else
+        if (s.front() == L'B') {
+            obj = new Button;
+            obj->initString = L"(new Button)";
+        } else
+        if (s.front() == L'L') {
+            obj = new Label;
+            obj->initString = L"(new Label)";
+        } else
+        if (s.front() == L'S') {
+            obj = new Slider;
+            obj->initString = L"(new Slider)";
+        } else {
+            return;
+        }
+
+        Prop p = nextProperty();
+
+        obj->setRect(Rect(p.x, p.y, p.w, p.h));
+        obj->setText(p.text);
+
+        editorSpace->add(obj);
+
+        if (s.front() != L';') {
+            while (s.front() == L'-') {
+                s.erase(0, s.find(L"->add(") + 6);
+
+                loadelem(obj);
+            }
+        }
+        s.erase(0, 1);
+
+    };
+
     void Editor::load() {
         control->clearBind();
         while (!editorSpace->getChilds().empty()) {
@@ -186,13 +296,48 @@ namespace MetaFrame {
 
         std::ifstream cin("sample.h");
 
+        //begin auto gen
 
+        std::string ss;
+        while (cin.eof()== false) {
+            std::string t;
+            cin >> t;
+            ss += t + " ";
+        }
+        
+        s.clear();
+        for (auto ch : ss) {
+            s.push_back(ch);
+        }
 
+        s.erase(0, s.find(L"begin auto gen") + 15);
+        s.erase(s.find(L"//end auto gen"), s.size() - s.find(L"//end auto gen"));
+             
 
+        Prop p = nextProperty();        
+        editorSpace->setRect(Rect(p.x, p.y, p.w, p.h));
 
+        if (s.front() != L';') {
+            while (s.front() == L'-') {
+                s.erase(0, s.find(L"->add(") + 6);
+
+                loadelem(editorSpace);
+            }
+        }
+        s.erase(0, 1);
+
+        editorSpace->build();
+        editorSpace->repaint();
 
         cin.close();
     }
+
+
+
+
+
+
+
 
     Editor::~Editor() {
 
